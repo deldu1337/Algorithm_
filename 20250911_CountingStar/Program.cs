bf31c6f84500c338b30d6862390fe2191e8d4a3c
JSH <deldu1337@naver.com>
2025-09-11 14:38:27 +0900

    2025-09-11

---
+++ 20250911_CountingStar/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _20250911_CountingStar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CountingStar("abc");
+        }
+
+        static void CountingStar(string s)
+        {
+            int[] result = new int[26];
+            foreach (int a in s)
+            {
+                result[a - 'a']++;
+            }
+
+            foreach (int a in result)
+                Console.Write($"{a} ");
+        }
+    }
+}
