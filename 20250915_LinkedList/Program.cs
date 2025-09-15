a1448603c0b48c4354b345c599e91e20de0e0614
JSH <deldu1337@naver.com>
2025-09-15 11:28:03 +0900

    2025-09-15

---
+++ 20250915_LinkedList/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace _20250915_LinkedList
+{
+    class Node<T>
+    {
+        public Node<T> Next;
+        public Node<T> Prev;
+        public T data;
+    }
+
+    class MyLinkedList<T>
+    {
+        public Node<T> Head = null;
+        public Node<T> Tail = null;
+        public int count = 0;
+
+        public Node<T> Add(T item)
+        {
+            Node<T> newNode = new Node<T>();
+            newNode.data = item;
+
+            if(Head == null)
+                Head = newNode;
+            if (Tail != null)
+            {
+                Tail.Next = newNode;
+                newNode.Prev = Tail;
+            }
+            Tail = newNode;
+            count++;
+            return newNode;
+        }
+
+        public void Remove(Node<T> node)
+        {
+            if(Head == node)
+                Head = Head.Next;
+            if(Tail == node)
+                Tail = Tail.Prev;
+            if (node.Next != null)
+                (node.Next).Prev = node.Prev;
+            if (node.Prev != null)
+                (node.Prev).Next = node.Next;
+            count--;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLinkedList<int> myLinkedList = new MyLinkedList<int>();
+            myLinkedList.Add(1);
+            Node<int> node = myLinkedList.Add(2);
+            myLinkedList.Add(3);
+            myLinkedList.Remove(node);
+
+            Console.WriteLine(myLinkedList.Head.data);
+            Console.WriteLine(myLinkedList.Head.Next.data);
+        }
+    }
+}
